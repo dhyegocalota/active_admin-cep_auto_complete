ebf48731fd51872ef85eb183777552b4e0388622
Dhyego Fernando <dhyegofernando@gmail.com>
2017-10-10 19:27:53 -0400

    Bump up to 0.2.0

--- lib/active_admin/cep_auto_complete/version.rb
+++ lib/active_admin/cep_auto_complete/version.rb
@@ -2,7 +2,7 @@ module ActiveAdmin
   module CepAutoComplete
     module Version
       MAJOR = 0
-      MINOR = 1
+      MINOR = 2
       PATCH = 0
       STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
     end
